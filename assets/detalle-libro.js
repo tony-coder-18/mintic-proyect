f1ecfe27af507f8bd3288f24ddf60410f9794bc9
Camila Gaviria <camilagb378@gmail.com>
2021-10-14 21:20:47 -0500

    buscar y descripcion del libro

---
+++ assets/detalle-libro.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    /* BOOTSNIPP FULLSCREEN FIX */
+    if (window.location == window.parent.location) {
+        $('#back-to-bootsnipp').removeClass('hide');
+        $('.alert').addClass('hide');
+    } 
+    
+    $('#fullscreen').on('click', function(event) {
+        event.preventDefault();
+        window.parent.location = "http://bootsnipp.com/iframe/Q60Oj";
+    });
+    
+    $('div > div.col-xl-2').on('click', function(event) {
+        event.preventDefault();
+        $('#myModal').modal('show');
+    })
+    
+    $('.btn-mais-info').on('click', function(event) {
+        $( '.open_info' ).toggleClass( "hide" );
+    })   
+});
